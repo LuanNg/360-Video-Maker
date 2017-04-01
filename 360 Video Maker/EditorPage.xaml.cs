366a37442aca7198d8220d2fd9b5b9649acff024
sandu vlad <sandu_vlad79@yahoo.com>
2017-04-01 23:36:38 +0300

    Ui Design

--- 360 Video Maker/EditorPage.xaml.cs
+++ 360 Video Maker/EditorPage.xaml.cs
@@ -16,15 +16,13 @@ using System.Windows.Shapes;
 namespace _360_Video_Maker
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for EditorPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class EditorPage : Page
     {
-        public MainWindow()
+        public EditorPage()
         {
             InitializeComponent();
-            WindowStartupLocation = WindowStartupLocation.CenterScreen;
-            MainFrame.Content = new EditorPage();
         }
     }
 }
--- 360 Video Maker/RenderPage.xaml.cs
+++ 360 Video Maker/RenderPage.xaml.cs
@@ -16,15 +16,13 @@ using System.Windows.Shapes;
 namespace _360_Video_Maker
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for RenderPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class RenderPage : Page
     {
-        public MainWindow()
+        public RenderPage()
         {
             InitializeComponent();
-            WindowStartupLocation = WindowStartupLocation.CenterScreen;
-            MainFrame.Content = new EditorPage();
         }
     }
 }
